cc589422a14d46f9cd9933089a1fe2e96e8790a4
Mohamed Hasir <mdhasir96@yahoo.com>
2023-11-02 08:24:43 +0530

    Worked out login page and side bar menu.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,19 +5,19 @@ import { UserAuth } from '../context/AuthContext';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('')
+  const [error, setError] = useState('');
   const { user, logIn } = UserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('')
+    setError('');
     try {
-      await logIn(email, password)
-      navigate('/')
+      await logIn(email, password);
+      navigate('/');
     } catch (error) {
       console.log(error);
-      setError(error.message)
+      setError(error.message);
     }
   };
 
@@ -25,15 +25,15 @@ const Login = () => {
     <div className='w-full h-screen'>
       <img
         className='hidden sm:block absolute w-full h-full object-cover'
-        src='https://images.unsplash.com/photo-1698749039505-63b2bd057477?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        src='https://wallpapercave.com/dwp1x/wp2943179.jpg'
         alt='/'
-        
       />
       <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
       <div className='fixed w-full px-4 py-24 z-50'>
-        <div className='max-w-[450px] h-[500px] mx-auto bg-black/75 text-white rounded-xl shadow-orange-right shadow-orange-bottom'>
+        <div className='max-w-[400px] h-[500px] mx-auto bg-black/50 text-white-100 rounded-xl shadow-orange-right shadow-orange-bottom shadow-orange-top shadow-orange-left'>
           <div className='max-w-[320px] mx-auto py-16 '>
-            <h1 className='text-3xl font-bold'>Sign In</h1>
+          <h1 className='text-3xl font-bold text-yellow-600'><center>WELCOME :)</center></h1>
+          <h1 className='text-3xl font-bold'>Sign In</h1>
             {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
             <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
               <input
@@ -53,7 +53,7 @@ const Login = () => {
               <button className='bg-orange-600 py-3 my-6 rounded font-bold text-black'>
                 Sign In
               </button>
-              <div className='flex justify-between items-center text-sm text-gray-600'>
+              <div className='flex justify-between items-center text-sm text-gray-400'>
                 <p>
                   <input className='mr-2' type='checkbox' />
                   Remember me
@@ -61,8 +61,8 @@ const Login = () => {
                 <p>Need Help?</p>
               </div>
               <p className='py-8'>
-                <span className='text-orange-600 '>New to Netflix?</span>{' '}
-                <Link to='/signup'>Sign Up</Link>
+              <span className='text-orange-600 px-1'>New to Netflix? </span>{' '}
+              <Link to='/signup' className="text-gray-400"> Sign Up</Link>
               </p>
             </form>
           </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -23,18 +23,20 @@ const Signup = () => {
       <div className='w-full h-screen'>
         <img
           className='hidden sm:block absolute w-full h-full object-cover'
-          src='https://images.unsplash.com/photo-1698749039505-63b2bd057477?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          src='https://wallpapercave.com/dwp1x/wp2943179.jpg'
           alt='/'
         />
-        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
+<div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
         <div className='fixed w-full px-4 py-24 z-50'>
-          <div className='max-w-[450px] h-[500px] mx-auto bg-black/75 text-white rounded-xl shadow-orange-right shadow-orange-bottom'>
+        <div className='max-w-[400px] h-[500px] mx-auto bg-black/50 text-white-100 rounded-xl shadow-orange-right shadow-orange-bottom shadow-orange-top shadow-orange-left'>
             <div className='max-w-[320px] mx-auto py-16'>
+            <h1 className='text-3xl font-bold text-yellow-600'><center>WELCOME :)</center></h1>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
               <form
                 onSubmit={handleSubmit}
                 className='w-full flex flex-col py-4'
               >
+
                 <input
                   onChange={(e) => setEmail(e.target.value)}
                   className='p-3 my-2 bg-white-700 rouded text-black'
@@ -52,9 +54,9 @@ const Signup = () => {
                 <button className='bg-orange-600 py-3 my-6 rounded font-bold'>
                   Sign Up
                 </button>
-                <div className='flex justify-between items-center text-sm text-gray-600'>
+                <div className='flex justify-between items-center text-sm text-gray-400'>
                   <p>
-                    <input className='mr-2' type='checkbox' />
+                    <input className='mr-4' type='checkbox' />
                     Remember me
                   </p>
                   <p>Need Help?</p>
@@ -63,7 +65,7 @@ const Signup = () => {
                   <span className='text-orange-600 px-1'>
                     Already subscribed to Netflix?
                   </span>{' '}
-                  <Link to='/login'>Sign In</Link>
+                  <Link to='/signup' className="text-gray-400">  Sign In</Link>
                 </p>
               </form>
             </div>
